be73e0934e41c33645f29514886a17adca2585d9
Damian Stasik <dstasik@student.agh.edu.pl>
2023-02-16 22:42:11 +0100

    Invoice class corrected. First test of InvoiceTest working

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,13 +1,14 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
     //private Collection<Product> products;
-    private Map<Product, Integer> products;
+    private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
         // TODO: implement
