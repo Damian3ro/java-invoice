9736d7776279577ca80bd56f23a10d144f991218
Damian3ro <97481302+Damian3ro@users.noreply.github.com>
2023-04-01 20:40:30 +0200

    Corrected getPriceWithTax() method and tax value in FuelCanister.java (#3)
    Merge tdd branch to master after FuelCanister.java correction

--- src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
@@ -3,7 +3,7 @@ package pl.edu.agh.mwo.invoice.product;
 import java.math.BigDecimal;
 
 public class FuelCanister extends Product {
-    private final BigDecimal additionalTaxValue = new BigDecimal("5.56");
+    private final BigDecimal additionalTaxValue = new BigDecimal(0);
 
     public FuelCanister(String name, BigDecimal price) {
         super(name, price, BigDecimal.ZERO);
@@ -15,6 +15,6 @@ public class FuelCanister extends Product {
 
     @Override
     public BigDecimal getPriceWithTax() {
-        return getPrice().multiply(getTaxPercent()).add(getPrice());
+        return getPrice().multiply(getTaxPercent()).add(getPrice()).add(additionalTaxValue);
     }
 }
