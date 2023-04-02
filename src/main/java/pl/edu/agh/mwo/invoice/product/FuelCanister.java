44a986a6d90a153dfc0653bc05254b4c9adda5a9
Damian3ro <97481302+Damian3ro@users.noreply.github.com>
2023-04-02 23:04:36 +0200

    Corrections for tax exemption on Mother-in-law Day
    * Corrected getPriceWithTax() method and tax value in FuelCanister.java
    
    * Corrections for tax exemption on Mother-in-law Day
    
    * Corrections for code formatting from checkstyle
    
    ---------
    
    Co-authored-by: Damian Stasik <dstasik@student.agh.edu.pl>

--- src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
@@ -1,20 +1,51 @@
 package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class FuelCanister extends Product {
-    private final BigDecimal additionalTaxValue = new BigDecimal(0);
+    private final BigDecimal exciseTaxValue = new BigDecimal("5.56");
+    private LocalDate currentDate = LocalDate.now();
+    private final int dayOfMotherInLawDay = 5;
+    private final int monthOfmotherInLawDay = 3;
 
     public FuelCanister(String name, BigDecimal price) {
-        super(name, price, BigDecimal.ZERO);
+        super(name, price, new BigDecimal("0.23"));
     }
 
-    public BigDecimal getAdditionalTaxValue() {
-        return additionalTaxValue;
+    public BigDecimal getExciseTaxValue() {
+        if (currentDate.getDayOfMonth() == dayOfMotherInLawDay
+                && currentDate.getMonthValue() == monthOfmotherInLawDay) {
+            return BigDecimal.ZERO;
+        }
+        return exciseTaxValue;
+    }
+
+    public LocalDate getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(int year, int month, int dayOfMonth) {
+        this.currentDate = LocalDate.of(year, month, dayOfMonth);
+    }
+
+    @Override
+    public BigDecimal getTaxPercent() {
+        if (currentDate.getDayOfMonth() == dayOfMotherInLawDay
+                && currentDate.getMonthValue() == monthOfmotherInLawDay) {
+            return BigDecimal.ZERO;
+        } else {
+            return super.getTaxPercent();
+        }
     }
 
     @Override
     public BigDecimal getPriceWithTax() {
-        return getPrice().multiply(getTaxPercent()).add(getPrice()).add(additionalTaxValue);
+        if (currentDate.getDayOfMonth() == dayOfMotherInLawDay
+                && currentDate.getMonthValue() == monthOfmotherInLawDay) {
+            return super.getPrice();
+        } else {
+            return super.getPriceWithTax().add(exciseTaxValue);
+        }
     }
 }
